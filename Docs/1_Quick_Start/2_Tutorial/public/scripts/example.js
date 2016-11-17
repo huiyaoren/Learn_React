21f19b137a12fb7d0dcb17e4c8e65b983d0e3c39
huiyaoren <wslshanlin@sina.com>
2016-11-17 14:40:23 +0800

    更新状态

--- Docs/1_Quick_Start/2_Tutorial/public/scripts/example.js
+++ Docs/1_Quick_Start/2_Tutorial/public/scripts/example.js
@@ -1,49 +1,9 @@
+//
+// var data1 = [
+//     {id: 1, author: "Pete Hunt", text: "* This is one comment."},
+//     {id: 2, author: "Jordan Hunt", text: "* This is another comment."}
+// ];
 
-var data = [
-    {id: 1, author: "Pete Hunt", text: "* This is one comment."},
-    {id: 2, author: "Jordan Hunt", text: "* This is another comment."}
-];
-
-// tutorial2.js
-var CommentList = React.createClass({
-    render: function(){
-        var commentNodes = this.props.data.map(function(comment){
-            return (
-                <Comment author={comment.author} key={comment.id}>
-                    {comment.text}
-                </Comment>
-            );
-        });
-        return (
-            <div className="commentList">
-                {commentNodes}
-            </div>
-        )
-    }
-});
-
-var CommentForm = React.createClass({
-    render: function(){
-        return(
-            <div>
-                Hello, world! I am a CommentForm.
-            </div>
-        )
-    }
-});
-
-// tutorial3.js
-var CommentBox = React.createClass({
-    render: function(){
-        return (
-            <div className="commentBox">
-                <h1>Comments</h1>
-                <CommentList data={this.props.data} />
-                <CommentForm />
-            </div>
-        );
-    }
-});
 
 var Comment = React.createClass({
     rawMarkup: function(){
@@ -63,7 +23,77 @@ var Comment = React.createClass({
     }
 });
 
+var CommentList = React.createClass({
+    render: function(){
+        var commentNodes = this.props.data.map(function(comment){
+            return (
+                <Comment author={comment.author} key={comment.id}>
+                    {comment.text}
+                </Comment>
+            );
+        });
+        return (
+            <div className="commentList">
+                {commentNodes}
+            </div>
+        )
+    }
+});
+
+
+var CommentForm = React.createClass({
+    render: function(){
+        return(
+            <div>
+                Hello, world! I am a CommentForm.
+            </div>
+        )
+    }
+});
+
+
+var CommentBox = React.createClass({
+    // 设置组件 State 的初始状态
+    getInitialState: function(){
+        return {data: []};
+    },
+
+    // ajax
+    loadCommentsFromServer: function(){
+        $.ajax({
+            url: this.props.url,
+            dataType: 'json',
+            cache: false,
+            success: function(data){
+                console.log(data);
+                this.setState({data: data});
+                console.log(this);
+            }.bind(this),
+            error: function(xhr, status, err){
+                console.error(this.props.url, status, err.toString());
+            }.bind(this)
+        });
+    },
+
+    // 获取数据
+    componentDidMount: function(){
+        this.loadCommentsFromServer()
+        setInterval(this.loadCommentsFromServer, this.props.pollInterval);
+    },
+
+    render: function(){
+        return (
+            <div className="commentBox">
+                <h1>Comments</h1>
+                <CommentList data={this.state.data} />
+                <CommentForm />
+            </div>
+        );
+    }
+});
+
+
 ReactDOM.render(
-    <CommentBox data={data} />,
+    <CommentBox  url="http://localhost/test.php" pollInterval={2000} />,
     document.getElementById('content')
 );
