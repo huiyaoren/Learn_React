c9bb50928ba5307adb9b61b5d756d13ea9d47d76
huiyaoren <wslshanlin@sina.com>
2016-11-17 11:45:45 +0800

    组件的属性

--- Docs/1_Quick_Start/2_Tutorial/public/scripts/example.js
+++ Docs/1_Quick_Start/2_Tutorial/public/scripts/example.js
@@ -1,12 +1,51 @@
-// tutorial1.js
 
+
+// tutorial2.js
+var CommentList = React.createClass({
+    render: function(){
+        return (
+            <div className="commentList">
+                <Comment author="Pete Hunt"> This is one comment</Comment>
+                <Comment author="Jordan Walke"> This is "another" comment</Comment>
+            </div>
+        )
+    }
+});
+
+var CommentForm = React.createClass({
+    render: function(){
+        return(
+            <div>
+                Hello, world! I am a CommentForm.
+            </div>
+        )
+    }
+});
+
+
+// tutorial3.js
 var CommentBox = React.createClass({
     render: function(){
         return (
             <div className="commentBox">
-                Hello, world! I am a CommentBox.
+                <h1>Comments</h1>
+                <CommentList />
+                <CommentForm />
             </div>
         );
+    }
+});
+
+var Comment = React.createClass({
+    render: function(){
+        return(
+            <div className="comment">
+                <h2 className="commentAuthor">
+                    {this.props.author}
+                </h2>
+                {this.props.children}
+            </div>
+        )
     }
 });
 
