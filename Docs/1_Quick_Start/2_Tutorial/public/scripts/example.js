f4ed2e1c0a5a58a87f24b3170896b447314ccab0
huiyaoren <wslshanlin@sina.com>
2016-11-17 13:30:46 +0800

    挂钩数据模型

--- Docs/1_Quick_Start/2_Tutorial/public/scripts/example.js
+++ Docs/1_Quick_Start/2_Tutorial/public/scripts/example.js
@@ -1,12 +1,22 @@
 
+var data = [
+    {id: 1, author: "Pete Hunt", text: "* This is one comment."},
+    {id: 2, author: "Jordan Hunt", text: "* This is another comment."}
+];
 
 // tutorial2.js
 var CommentList = React.createClass({
     render: function(){
+        var commentNodes = this.props.data.map(function(comment){
+            return (
+                <Comment author={comment.author} key={comment.id}>
+                    {comment.text}
+                </Comment>
+            );
+        });
         return (
             <div className="commentList">
-                <Comment author="Pete Hunt"> This is one comment</Comment>
-                <Comment author="Jordan Walke"> This is "another" comment</Comment>
+                {commentNodes}
             </div>
         )
     }
@@ -22,14 +32,13 @@ var CommentForm = React.createClass({
     }
 });
 
-
 // tutorial3.js
 var CommentBox = React.createClass({
     render: function(){
         return (
             <div className="commentBox">
                 <h1>Comments</h1>
-                <CommentList />
+                <CommentList data={this.props.data} />
                 <CommentForm />
             </div>
         );
@@ -37,19 +46,24 @@ var CommentBox = React.createClass({
 });
 
 var Comment = React.createClass({
+    rawMarkup: function(){
+        var md = new Remarkable();
+        var rawMarkup = md.render(this.props.children.toString());
+        return { __html: rawMarkup };
+    },
     render: function(){
         return(
             <div className="comment">
                 <h2 className="commentAuthor">
                     {this.props.author}
                 </h2>
-                {this.props.children}
+                <span dangerouslySetInnerHTML={this.rawMarkup()} />
             </div>
         )
     }
 });
 
 ReactDOM.render(
-    <CommentBox />,
+    <CommentBox data={data} />,
     document.getElementById('content')
 );
